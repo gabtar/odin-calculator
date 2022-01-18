44acc25c41b87bef5fa7481e7c95d47ebb1db803
Gabriel Tarifa <gabrieltarifa@gmail.com>
2022-01-18 19:50:22 -0300

    Finished, added keyboard support, more sytles and some fixes

--- js/calculator.js
+++ js/calculator.js
@@ -1,46 +1,139 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  // Number input
   document.querySelectorAll('.number').forEach(function (number) {
     number.addEventListener('click', () => {
-      setDisplayValue(getDisplayValue() + number.value)
+      enterNumber(number.value);
     }); 
   });
 
+  // Operation input
   document.querySelectorAll('.operator').forEach(function (operation) {
     operation.addEventListener('click', () => {
-      // If it's stored a previous value, just calculate operations so far and store the new value
-      if(displayValue) {
-        setDisplayValue(calculate(operation.id, displayValue, getDisplayValue()))
-      }
-      displayValue = getDisplayValue();
-      setDisplayValue("");
-      console.log(displayValue);
-    });
-
+      enterOperation(operation.id);
+    }); 
   });
 
-  document.querySelector('.clear').addEventListener('click', () => {
-    displayValue = "";
-    setDisplayValue("");
+  // Clear input
+  document.querySelector('.clear').addEventListener('click', clear);
+
+  // Undo last number pressed
+  document.querySelector('.backspace').addEventListener('click', () => {
+    setDisplayValue(getDisplayValue().slice(0,-1));
+  });
+
+  // Dot input
+  document.querySelector('.dot').addEventListener('click', dot);
+
+  // Sign input
+  document.querySelector('.sign').addEventListener('click', () => {
+    if(getDisplayValue().indexOf("-") == -1) {
+      setDisplayValue("-" + getDisplayValue());
+    } else {
+      setDisplayValue(getDisplayValue().slice(1)); 
+    }
+  });
+
+  // Keyboard support
+  document.addEventListener("keydown", (event) => {
+    switch(event.key) {
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        enterNumber(event.key);
+        break;
+      case "+":
+        enterOperation("add");
+        break;
+      case "-":
+        enterOperation("subtract");
+        break;
+      case "*":
+        enterOperation("multiply");
+        break;
+      case "/":
+        enterOperation("divide");
+        break;
+      case "Enter":
+        enterOperation("equals");
+        break;
+      case "Delete":
+        clear()
+        break;
+      case "Backspace":
+        undo()
+        break;
+      case ".":
+        dot()
+        break;
+    }
   });
 
 });
 
-let displayValue = ""; // When any operation button is pressed, the current display value will be sotred here!
+// Globals
+let lastResult = ""; // When any operation button is pressed, the current display value or the result calculation will be sotred here!
+let currentOperation = ""; 
+let lastOperation = "";
 
-// Operations available
+// Operations availables
 const operations = {
   add: (numberOne, numberTwo) => numberOne + numberTwo,
   subtract: (numberOne, numberTwo) => numberOne - numberTwo,
   multiply: (numberOne, numberTwo) => numberOne * numberTwo,
   divide: (numberOne, numberTwo) => numberOne / numberTwo,
-  sqrt: (numberOne) => Math.sqrt(numberOne),
-  power: (numberOne, numberTwo) => Math.pow(numberOne, numberTwo)
+  power: (numberOne, numberTwo) => Math.pow(numberOne, numberTwo),
+  equals: () => lastResult
 }
 
+// Functions
 const getDisplayValue = () => document.getElementById('display').value;
-const setDisplayValue = (number) => document.getElementById('display').value = number;
+const setDisplayValue = number => document.getElementById('display').value = number;
+const setPlaceholder = value => document.getElementById('display').placeholder = value;
 
-const calculate = (operation, numberOne, numberTwo = 0 ) => {
-  return operations[operation](parseInt(numberOne),parseInt(numberTwo));
+const calculate = (operation, numberOne = 0, numberTwo = 0) => {
+  return operations[operation](parseFloat(numberOne),parseFloat(numberTwo)); 
 }
+
+const enterNumber = (number) => {
+  // Limit to 15 digits
+  if(getDisplayValue().length <= 15) {
+    setDisplayValue(getDisplayValue() + number);
+  }
+}
+
+const enterOperation = (operation) => {
+  if(getDisplayValue() || lastOperation == 'equals') {
+    currentOperation = operation
+    // If there is an operation in progress, calculate the last result using old stored value
+    if(lastResult) {
+      lastResult = lastOperation == 'equals' ? lastResult : calculate(lastOperation , lastResult, getDisplayValue());
+    } else {
+      lastResult = getDisplayValue();
+    }
+    setPlaceholder(lastResult);
+    setDisplayValue("");
+    lastOperation = currentOperation;
+  }
+}
+
+const clear = () => {
+    lastResult = "";
+    setDisplayValue("");
+    setPlaceholder("0");
+};
+
+const undo = () => setDisplayValue(getDisplayValue().slice(0,-1));
+
+const dot = () => {
+  if(getDisplayValue().indexOf(".") == -1) {
+    setDisplayValue(getDisplayValue() + ".");
+  }
+} 
